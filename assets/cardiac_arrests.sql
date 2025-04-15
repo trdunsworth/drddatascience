1c70a39955255f47c60c47e5afb64adbc31182ba
Tony Dunsworth <tony.dunsworth@gmail.com>
2025-04-15 12:30:57 -0400

    20250415 First Commit

--- assets/cardiac_arrests.sql
+++ assets/cardiac_arrests.sql
@@ -8,13 +8,15 @@ SET @time1 = '2025-01-01 00:00:00.0000000';
 SELECT Master_Incident_Number,
     Response_Date,
     Address,
+    Latitude,
+    Longitude,
     Time_PhonePickUp,
     Time_FirstCallTakingKeystroke,
     Time_CallEnteredQueue,
     Time_First_Unit_Assigned,
     Time_CallTakingComplete
 FROM Response_Master_Incident
-WHERE Response_Date BETWEEN DATEADD(YEAR, -5, @time1) AND @time1
+WHERE Response_Date > @time1
     AND Problem LIKE 'CARDIAC ARREST%'
     AND Master_Incident_Number != ''
 ORDER BY Response_Date;
